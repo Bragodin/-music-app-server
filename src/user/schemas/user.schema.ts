83ca86d767d56fe8a1465c328dacd8739066268f
Bragodin <drozdegor.sr@gmail.com>
2020-06-08 13:22:41 +0300

    add user

---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,33 @@
+import * as mongoose from 'mongoose';
+import { roleEnum } from '../enums/role.enum';
+
+export const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    default: null,
+    required: false
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: [String],
+    required: true,
+    enum: Object.values(roleEnum)
+  }
+});
+
+UserSchema.index({ email: 1 }, {unique: true });
+
+const User = mongoose.model("User", UserSchema);
+
+
---
+++ src/user/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
+import * as _ from 'lodash';
+
+import { IUser } from './interfaces/user.interface';
+import { CreateUserDto } from './dto/create-user.dto';
+
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel('User') private readonly  userModel: Model<IUser>) {}
+
+  async create(createUserDto: CreateUserDto, role: Array<string>): Promise<IUser> {
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(createUserDto.password, salt);
+    const createdUser = new this.userModel(_.assignIn(createUserDto, { password: hash, role}));
+    return await createdUser.save();
+  }
+
+  async  find(id: string):  Promise<IUser>{
+    return await this.userModel.findById(id).exec();
+  }
+}
